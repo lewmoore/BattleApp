03e8dbeb41103efe6f7447ec05a1141e641a2f8e
Lewis Moore <lewis.moore2707@gmail.com>
2018-01-16 22:04:45 +0000

    Added basic test for player hit points and passing

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -9,3 +9,15 @@ feature "Enter player names" do
     expect(page).to have_text("Lewis is Player 1 and Dave is Player 2")
   end
 end
+
+feature "View hitpoints" do
+  scenario "Can see Player 2's hitpoints" do
+    visit('/new_player')
+    fill_in :player_1, with: 'Lewis'
+    fill_in :player_2, with: 'Dave'
+    click_button 'Submit'
+
+    expect(page).to have_text 'Dave: 100 Hitpoints'
+
+  end
+end
